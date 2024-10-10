aece7af78737a29a861b18217bc743909de4297e
Konstantin <ref6512722@gmail.com>
2024-10-10 16:27:09 +0300

    string info

--- module_3_1.py
+++ module_3_1.py
@@ -1,3 +1,17 @@
 calls = 0
-def count_calls():
-    calls += 1
+string = input("Введите строку :")
+print(string)
+
+
+# def count_calls(calls):
+#     calls += 1
+
+
+def string_info(string):
+    lenght = len(string)
+    hi = string.upper()
+    low = string.lower()
+    print(lenght, hi, low)
+
+
+string_info(string)
