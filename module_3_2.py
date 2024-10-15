310c819ea90d720606a8dd618aa5b6631faa813a
Konstantin <ref6512722@gmail.com>
2024-10-15 10:16:33 +0300

    start

---
+++ module_3_2.py
@@ -0,0 +1,10 @@
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+
+
+
+
+
+send_email('Это сообщение для проверки связи', 'm1525@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', 'urban.info@gmail.com')
+send_email('Пожалуйста исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
