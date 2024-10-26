50421b3ef8eb58f0b8d0d6f6e66d4689242a0dae
Konstantin <ref6512722@gmail.com>
2024-10-26 11:38:15 +0300

    01

--- module_3_3.py
+++ module_3_3.py
@@ -5,7 +5,12 @@ def print_params(a=1, b='Строка', c=True):
 print_params()
 print_params(True, 5.3, 9)
 print_params(b=25)
-print_params(c=[1, 2, 3])
+print_params(c= (1, 2, 3))
 
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
+
+values_list = [1, 'Строка', True]
+values_dict = {'a':5, 'b':'Строка 2', 'c':False}
+print_params(*values_list)
+print_params(**values_dict)
